93198a05e69ace3f4b313b50a8b490baa3cd35c1
Gorakh Raj Joshi <59221046+gorakhjoshi@users.noreply.github.com>
2021-07-13 15:33:31 +0000

    Rendered map using geolocation API

--- script.js
+++ script.js
@@ -10,3 +10,32 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(function (position) {
+    // console.log(position);
+
+    const { latitude, longitude } = position.coords;
+    // console.log(latitude);
+
+    console.log(`https://www.google.com/maps/@${latitude},${longitude}`)
+
+    const coords = [latitude, longitude]
+
+    const map = L.map('map').setView(coords, 13);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    L.marker(coords).addTo(map)
+      .bindPopup('This is a marker')
+      .openPopup();
+
+
+  }
+    , function () {
+      console.log('Can not access your coordinates')
+    })
+}
