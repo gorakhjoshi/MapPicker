1e62414ba93582489812254f8eec0a0dc4f329b5
Gorakh Raj Joshi <59221046+gorakhjoshi@users.noreply.github.com>
2021-07-14 15:20:27 +0000

    Woking on marker and form

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+let map, mapEvent;
 
 if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(function (position) {
@@ -23,19 +24,41 @@ if (navigator.geolocation) {
 
     const coords = [latitude, longitude]
 
-    const map = L.map('map').setView(coords, 13);
+    map = L.map('map').setView(coords, 13);
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
 
-    L.marker(coords).addTo(map)
-      .bindPopup('This is a marker')
-      .openPopup();
+
+
+    // console.dir(map)
+    map.on('click', function (mapE) {
+      mapEvent = mapE;
+      form.classList.remove('hidden');
+      inputDistance.focus();
+
+
+    })
 
 
   }
     , function () {
       console.log('Can not access your coordinates')
     })
+
 }
+
+form.addEventListener ( 'submit', function (e) {
+
+    e.preventDefault();
+
+    inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value = '';
+
+    const { lat, lng } = mapEvent.latlng;
+
+    L.marker([lat, lng]).addTo(map)
+      .bindPopup(L.popup({ maxWidth: 300, minWidth: 150, autoClose: false, closeOnClick: false, className: 'running-popup' })).setPopupContent('this is popup')
+      .openPopup();
+  
+});
